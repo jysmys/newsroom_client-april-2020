83055c93cef8e01267a7236a657e8dca38117663
japh <jenny.scherr@svenskaspel.se>
2020-06-01 11:56:59 +0200

    update axios for deployment

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
-import { StripeProvider } from 'react-stripe-elements'
+import { StripeProvider } from "react-stripe-elements";
 import "./css/index.css";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://newsroom-api.herokuapp.com/api";
 
 ReactDOM.render(
   <StripeProvider apiKey="pk_test_21nBNjeqdyB1Mzm2VjDPQprF00kyEKYZSK">
